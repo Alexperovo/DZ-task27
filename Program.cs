454cf84db764a8c84294c3c35726765f5fb8fb3c
Alexperovo <alexperovo@gmail.com>
2022-11-22 00:58:06 +0300

    не работает

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿        // Напишите программу, которая принимает на вход  число и выдает сумму цифр в числе
+        void fun(int A)
+        {
+            int b = 0;
+            if (A > 0)
+                for (int i = 1; i >= A; i++)
+                {
+                    b += i;
+                }
+             else
+                  for (int i =1; i >= A; i--)
+                  {
+                    b += i;
+                  }
+                  Console.WriteLine(b);
+        }
+        Console.WriteLine("Введите число: ");
+
+        fun(Convert.ToInt32(Console.ReadLine()));
